9ae49949b50db876709839bea2c3d90b113dcd9a
pomfy113 <pomfy113@gmail.com>
2017-09-21 19:26:07 -0700

    Mongol + New Ratings + Extra Credit

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 var express = require('express')
 var app = express()
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // var reviews = [
 //   { title: "Great Review" },
 //   { title: "Next Review" },
 //   { title: "Okay Review"}
 // ]
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes');
-
-var Review = mongoose.model('Review', {
-  title: String
-});
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Index
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+
+var Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String,
+  description: String,
+  rating: Number
+});
+
 app.get('/', function (req, res) {
-    Review.find(function(err, reviews){
-        res.render('reviews-index', {reviews: reviews});
-    })
+  Review.find(function(err, reviews) {
+    res.render('reviews-index', {reviews: reviews});
+  })
+})
+
+app.post('/reviews', function (req, res) {
+  console.log(req.body);
+  res.redirect('/')
+})
+// NEW
+app.get('/reviews/new', function (req, res) {
+  res.render('reviews-new', {});
 })
 
 app.listen(3000, function () {
